00183e246e40051395a012422fa7b02d78ae3078
Renee Salz <rsalz@renees-mbp.umcn.nl>
2019-05-21 11:24:08 +0200

    added

---
+++ customized_sequence_creation/misc_scripts/diff.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python2
+
+import re, collections, sys
+from sets import Set
+
+'''Identify proteins that have differences between the reference and the customized sequences'''
+
+def print_diff_lists(reference,custom,outfa,outfahet):
+    '''show differences between the reference and the custom predicted protein sequences
+    categories: those that reference matches one of the haplotypes, reference matches none of the haplotypes, reference does not match single sequence (het vars absent)'''
+    reference_seqs={} #store id:sequence
+    with open(reference) as ref:
+        for line in ref:
+            if line.startswith('>'):
+                id=re.split('\|',line,1)[0]
+            elif line.strip!='':
+                reference_seqs[id]=line.strip()
+    f=open(outfa,'w')
+    fh=open(outfahet,'w')
+    het=id_het(custom)
+    with open(custom) as cu:
+        for line in cu:
+            if line.startswith('>'):
+                id=re.split('\|',line,1)[0]
+                idline=line
+            elif line.strip!='':
+                if reference_seqs[id]!= line.strip():
+                    if id in het:
+                        fh.writelines(idline+line)
+                    else:
+                        f.writelines(idline+line)
+    return 'Fastas written'
+                    
+def id_het(customfa):
+    het=Set()
+    with open(customfa) as handle:
+        previd=''
+        for line in handle:
+            if line.startswith('>'):
+                id=re.split('\|',line,1)[0]
+                if id==previd:
+                    het.add(id)
+                previd=id
+    return het

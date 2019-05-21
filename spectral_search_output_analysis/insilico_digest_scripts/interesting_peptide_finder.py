200ccb7b7f0b7f29fd976c20bff9f1514f1fe5a9
Renee Salz <rsalz@renees-mbp.umcn.nl>
2019-05-21 17:44:18 +0200

    bug fix insilico_digest_diff

--- spectral_search_output_analysis/insilico_digest_scripts/interesting_peptide_finder.py
+++ spectral_search_output_analysis/insilico_digest_scripts/interesting_peptide_finder.py
@@ -11,36 +11,38 @@ def insilico_digest_diff(cpdtref,cpdtcustom):
     custom=read_cpdt(cpdtcustom)
     newall={}
     newsnv={}
-    for id,peplist in ref.iteritems():
-        idalt=id+' h1'
-        if id in custom or idalt in custom:
-            if id in custom:
-                key=id
-            else:
-                key=idalt
-            c_peplist=custom[key]
-            dif=c_peplist.difference(peplist)
-            for pep in dif:
-                if determine_snv(pep,peplist):
-                    if key in newsnv:
-                        newsnv[key].add(pep)
+    for pid,peplist in ref.iteritems():
+        idalt=pid+' h1'
+        keys=[]
+        if idalt in custom:
+            keys=[pid,idalt]
+        elif pid in custom:
+            keys=[pid]
+        if len(keys)>0:
+            for key in keys:
+                c_peplist=custom[key]
+                dif=c_peplist.difference(peplist)
+                for pep in dif:
+                    if determine_snv(pep,peplist):
+                        if key in newsnv:
+                            newsnv[key].add(pep)
+                        else:
+                            newsnv[key]=Set()
+                            newsnv[key].add(pep)
+                    if key in newall:
+                        newall[key].add(pep)
                     else:
-                        newsnv[key]=Set()
-                        newsnv[key].add(pep)
-                if key in newall:
-                    newall[key].add(pep)
-                else:
-                    newall[key]=Set()
-                    newall[key].add(pep)
+                        newall[key]=Set()
+                        newall[key].add(pep)
     return newall,newsnv
 
-def determine_snv(peptide,list):
+def determine_snv(peptide,plist):
     ''' checks whether the peptide in question differs from a member in the list by exactly 1 amino acid
     input: a peptide and a list of peptides
     output: boolean
     '''
     mismatch=0
-    for pep in list:
+    for pep in plist:
         if len(pep)==len(peptide):
             for idx,aa in enumerate(pep):
                 if aa!=peptide[idx]:
@@ -66,16 +68,16 @@ def read_cpdt(cpdt):
                 cpdt_pep[key].add(lp)
     return cpdt_pep
 
-def write_cpdt(dict,outfile):
+def write_cpdt(d,outfile):
     '''
     input format: {id:Set(peptides)}
     output: new cpdt file
     '''
     f=open(outfile,'w')
-    for id,peplist in dict.iteritems():
-        f.writelines(id+'\n')
+    for id,peplist in d.iteritems():
+        f.writelines('>'+id+'\n')
         for pep in peplist:
-            f.writelines('\t'+'PEPTIDE '+pep)
+            f.writelines('\t'+'PEPTIDE '+pep+'\n')
     return 'new CPDT file written to '+outfile
 
 alldiffpeps,snvdiffpeps=insilico_digest_diff(sys.argv[1],sys.argv[2])

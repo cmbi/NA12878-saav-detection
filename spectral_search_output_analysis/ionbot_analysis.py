038928114ec57ed4e61d4b91db62b3263fd5b42a
Renee Salz <rsalz@renees-mbp.umcn.nl>
2019-05-23 14:56:10 +0200

    functions initiated

--- spectral_search_output_analysis/ionbot_analysis.py
+++ spectral_search_output_analysis/ionbot_analysis.py
@@ -6,21 +6,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
+from sets import Set
 from collections import Counter
 
 # Set the visualization settings
 matplotlib.rcParams['axes.titlesize'] = 'xx-large'
 matplotlib.rcParams['axes.labelsize'] = 'x-large'
 matplotlib.rcParams['figure.figsize'] = (20.0, 10.0)
-from sets import Set
 
 '''
 this script will concatenate all the separate spectral match files generated from ionbot and process them to answer research questions
 
 questions:
 - horizontal/vertical coverage of the whole proteome?
-- extent of multiple mapping
--
+- extent of multiple mapping, proportion of mapping that is exclusive to the long-read transcriptome
+- are my peptides of interest present? How often does ionbot correctly predict an SNV?
 '''
 def concatenate_csvs(folder_path):
     directory= os.fsencode(csvpath)
@@ -75,7 +75,22 @@ def import_coding_transcriptids(sources):
                         transcript_ids.append(line.strip()[1:])
     return(transcript_ids)
 
-def import_cpdt(cpdt):
+def detected_proteins(hits_df):
+    proteins_covered=Counter()
+    for row in ib_mutout_human_ont.iterrows():
+        protein=''
+        if '||' in row[1][9]:
+            ids=row[1][9].split('||')
+            for idu in ids:
+                proteins_covered[idu]+=1
+        else:
+            proteins_covered[row[1][9]]+=1
+    return(proteins_covered)
+
+def import_cpdt(cpdt, proteins_covered):
+    ''' read the cpdt files into a data structure
+    importing only the peptides from proteins that were matched with ionbot (in the sample)
+    '''
     cpdt_pep={}
     full_seqs={}
     with open(cpdt) as c:
@@ -98,8 +113,55 @@ def import_cpdt(cpdt):
     return(cpdt_pep, full_seqs)
 
 
+def bin_hits_by_source(df):
+    '''sort peptide hits by their source'''
+    ref_only=set()
+    ont_only=set()
+    #ont_only_prot={}
+    both=set()
+    for row in ib_mutout_human_ont.iterrows():
+        if '||' in row[1][9]:
+            ids=row[1][9].split('||')
+            ont=False
+            ref=False
+            for i in ids:
+                if '|m.' in i: #assumes that the proteins from the reference were not generated with ANGEL
+                    ont=True
+                else:
+                    ref=True
+            if ont and ref:
+                both.add(row[1][0])
+            elif ont:
+                ont_only.add(row[1][0])
+                #ont_only_prot[row[1][0]]=row[1][9]
+            elif ref:
+                ref_only.add(row[1][0])
+            else:
+                raise Exception('')
+        else:
+            if 'HUMAN' in (row[1][9]):
+                ref_only.add(row[1][0])
+            else:
+                ont_only.add(row[1][0])
+    return(ref_only,ont_only,both)
 
-    
+def plot_source_piechart():
+    '''this function will plot the source piechart and save it to a pdf'''
+    return 0
+
+def plot_coverage_plots():
+    '''this function will plot the graphs that correspond to the coverage of the proteome'''
+    return 0
+
+def make_report(hits_df):
+    '''this function will output a report with general information collected about the hits
+    which includes:
+    - how many unique peptides are found
+    - how many unique proteins are found (and how many are in transcriptome v reference)
+    - inventory PTMs (including how many SNVs there are)
+    '''
+    return 0
+
     
     
     

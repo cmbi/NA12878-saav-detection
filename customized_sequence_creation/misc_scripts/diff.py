ca650f08d4c177055ea929506e1bc813ee6a31c3
Renee Salz <rsalz@renees-mbp.umcn.nl>
2019-05-28 16:52:47 +0200

    categorization and hap info added

--- customized_sequence_creation/misc_scripts/diff.py
+++ customized_sequence_creation/misc_scripts/diff.py
@@ -2,11 +2,13 @@
 
 import re, collections, sys
 from sets import Set
+from lib2to3.tests.data import different_encoding
 
 '''
 
 After creating the customized sequences and putting them through ORF prediction
 Identify proteins that have differences between the reference and the customized sequences
+Quantify interesting outcomes on the protein level due to variations on the genome level such as intron insertions, exon skipping, reading frame shifts, alternative start site, etc
 
 For the gencode: although a file with predicted protein sequences is provided by gencode and this will likely be the same as predicting with ANGEL, 
 it is most ideal to compare ANGEL predicted sequences to other ANGEL predicted sequences to rule out the possibility that sequence prediction played 
@@ -17,14 +19,13 @@ sequence files only
 
 def print_diff_lists(reference,custom,outfa,outfahet):
     '''show differences between the reference and the custom predicted protein sequences
-    
-    categories: those that reference matches one of the haplotypes, reference matches none of the haplotypes, reference does not match single sequence (het vars absent)
-    assumes that both files originate from ANGEL, but the gencode and flair sequences have a different identifier format (hence isONT)
-    assumes that the * has been removed from the ANGEL peptide sequence predictions
-    
-    input: reference peptide sequneces (ANGEL output), customized peptide sequences (ANGEL output), output fasta name, output fasta name (heterozygous)
-    output: fastas containing the identifier (from the customized sequences), with reference sequence listed first and then customized protein sequence
-    '''
+    categories: those that reference matches one of the haplotypes, reference matches none of the haplotypes, reference does not match single sequence (het vars absent)'''
+    altstart=0
+    altend=0
+    frameshift=0
+    SNP=0
+    different=open('different.fa','w')
+    undef=open('uncat.fa', 'w')
     reference_seqs={} #store id:sequence
     with open(reference) as ref:
         for line in ref:
@@ -34,34 +35,109 @@ def print_diff_lists(reference,custom,outfa,outfahet):
                 reference_seqs[tid]=line.strip()
     f=open(outfa,'w')
     fh=open(outfahet,'w')
-    het=id_het(custom)
     with open(custom) as cu:
         for line in cu:
             if line.startswith('>'):
                 id=re.split('\|',line,1)[0]
+                het=False
+                if '_h' in id:
+                    id=id.split('_h')[0]
+                    het=True
                 idline=line
             elif line.strip!='':
-                if id in reference_seqs: #theoretically should always be true, but ANGEL orf predictor predicted some sequences in the custom set that it didn't in the reference set. This issue is under investigation
+                if id in reference_seqs:
                     if reference_seqs[id]!= line.strip():
-                        if id in het:
+                        category=identify_interesting_events(reference_seqs[id], line.strip())
+                        if category==''
+                        if het:
                             fh.writelines(idline+reference_seqs[id]+'\n'+line)
                         else:
                             f.writelines(idline+reference_seqs[id]+'\n'+line)
     return 'Fastas written'
-                    
-def id_het(customfa):
-    '''sequences with heterozygous variants will have 2 sequences with the same id, save these identifiers'''
-    het=Set()
-    with open(customfa) as handle:
-        recorded=Set()
+
+def identify_interesting_events(reference,custom):
+    ''' input reference sequence and custom sequence and classify the event that occurred
+    possible categories:
+        - alternative start
+        - alternative end
+        - indel
+        - frame shift
+        - SNP
+        - other different
+        - undefined
+    use align when necessary?
+    '''
+    if len(reference)==len(custom):
+        consecutive=0
+        streak=[]
+        mismatch=0
+        for idx,aa in enumerate(reference):
+            if aa!=custom[idx]:
+                consecutive+=1
+                mismatch+=1
+            else:
+                if consecutive>1:
+                    streak.append(consecutive)
+                consecutive=0
+        if consecutive>1:
+            streak.append(consecutive)
+        if consecutive>15:
+            return "frameshift"
+        if len(streak)>0 and sum(streak)>20:
+            return 'different'
+        elif len(streak)==0 and mismatch>0:
+            return 'SNP'
+    elif len(reference)>len(custom): #custom shorter
+        if custom in reference:
+            rest=reference.split(custom)
+            if rest[0]=='':
+                return "alternative end"
+            elif rest[1]=='':
+                return "alternative start"
+        elif len(custom)>15: #this is the tricky part, conservative tag based search
+            return tag_based_search(custom, reference)
+    elif len(custom)>len(reference): #reference shorter
+        if reference in custom:
+            rest=custom.split(reference)
+            if rest[0]=='':
+                return "alternative end"
+            elif rest[1]=='':
+                return "alternative start"
+        elif len(reference)>15:
+            return tag_based_search(reference, custom)
+    return event
+
+def tag_based_search(shorter,longer):
+    '''in the case where imperfect match between 2 varied length strings, use tag based search'''
+    if shorter[:6] and shorter[-6:] in longer: #in case the shorter is completely inside the longer
+        head=re.split(shorter[:6],longer,1)
+        tail=head[1].split(shorter[-6:])
+        if len(tail)>2: #if the end pattern 
+            tail=[''.join(tail[:-1]),tail[-1]]
+        if len(head[0])<3 and len(tail[1])<3: #something in the middle that is different
+            return "indel"
+        elif len(head[0])>3 and abs(head[1]-len(shorter))<3:
+            return "alternative start"
+        elif len(tail[-1])>3 and abs(len(head[0])+len(tail[0])-len(shorter))<3:
+            return "alternative end"
+    return "undefined" #doesn't deal with SNPs in the tags
+
+def het_diff_info(difffile):
+    '''check how many sequences had only one haplotype that was different from the reference rather than both
+    input: the het file generated from print_diff_lists function'''
+    onehap=Set()
+    twohap=Set()
+    with open(difffile) as handle:
         for line in handle:
             if line.startswith('>'):
-                id=re.split('\|',line,1)[0]
-                if id in recorded:
-                    het.add(id)
-                recorded.add(id)
-    return het
-
+                tid=re.split('_h',line,1)[0]
+                if tid in onehap:
+                    onehap.remove(tid)
+                    twohap.add(tid)
+                else:
+                    onehap.add(tid)
+    return str(len(onehap))+' sequences had one haplotype differing from the reference while '+str(len(twohap))+' had both haplotypes differing from the reference'
+    
 print print_diff_lists(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4])
 
 

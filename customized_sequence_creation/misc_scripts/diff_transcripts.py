62e74b3bb82e04282b725f8220b5bdb26a8202bb
Renee Salz <rsalz@renees-mbp.home>
2019-06-01 22:30:18 +0200

    created transcript diff script

---
+++ customized_sequence_creation/misc_scripts/diff_transcripts.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python2
+
+import re, collections, sys
+from sets import Set
+
+'''This script will help determine how many sequences were affected by the customized sequences
+'''
+
+def compare_transcripts(fasta_custom, fasta_reference):
+    '''input transcript fastas from the custom and reference'''
+    ref=import_fasta(fasta_reference)
+    unmatching=Set()
+    with open(fasta_custom) as handle:
+        seq=''
+        for line in handle:
+            if line.startswith('>'):
+                if seq!='':
+                    if '_h' in tid:
+                        unmatching.add(tid.split('_')[0])
+                    elif tid in ref:
+                        if seq!=ref[tid]:
+                            unmatching.add(tid)
+                    else:
+                        raise KeyError('IDs of reference and custom transcript sets dont match')
+                    seq=''
+                if '|' in line:
+                    line=line.split('|')[0]
+                tid=line.strip()[1:]
+            else:
+                seq+=line.strip()
+    return len(unmatching)
+
+def import_fasta(fastafile):
+    fasta={}
+    with open(fastafile) as handle:
+        seq=''
+        for line in handle:
+            if line.startswith('>'):
+                if seq!='':
+                    fasta[tid]=seq
+                    seq=''
+                tid=line.strip()[1:]
+            else:
+                seq+=line.strip()
+    return fasta

7233458e5cae4a7469cc2ae5eadb4d626cc139db
RalfG <ralfg@hotmail.be>
2019-08-08 17:58:45 +0200

    small changes

--- spectral_search_output_analysis/ionbot_output_analysis/ionbot_analysis.py
+++ spectral_search_output_analysis/ionbot_output_analysis/ionbot_analysis.py
@@ -6,6 +6,7 @@ import calculations
 import helper_functions
 import file_import
 import plots
+import fdr_reestimation
 
 
 '''
@@ -18,27 +19,25 @@ def main(args):
     '''
     #import ionbot output data
     print("importing ionbot results")
-    ibdf_ontonly=file_import.concatenate_csvs(args['ont'])
-    ibdf_refonly=file_import.concatenate_csvs(args['ref'])
-    ibdf_combi=file_import.concatenate_csvs(args['cvf'])
-    ibdf_combi_pg=file_import.concatenate_csvs(args['cvc'])
+    ibdf_ontonly_original=file_import.concatenate_csvs(args['ont'])
+    ibdf_refonly_original=file_import.concatenate_csvs(args['ref'])
+    ibdf_combi_original=file_import.concatenate_csvs(args['cvf'])
+    ibdf_combi_pg_original=file_import.concatenate_csvs(args['cvc'])
 
-    #QC and FDR re-estimation
+    #inital QC
     print("plotting initial QC")
-    plots.plot_scores(ibdf_ontonly.dropna(),ibdf_refonly.dropna(),ibdf_combi.dropna())
-    plots.plot_scores_pg(ibdf_combi.dropna(),ibdf_combi_pg.dropna())
-    plots.plot_target_decoy(ibdf_combi.dropna(),"qc_pearsonr_decoy_varfree.png", plot_title="Search result variant-free")
-    plots.plot_target_decoy(ibdf_combi_pg.dropna(),"qc_pearsonr_decoy_varcont.png", plot_title="Search result variant-containing")
-    plots.plot_qvalues_comparison({'ONT only':ibdf_ontonly,'Ref only':ibdf_refonly,'Combi variant-containing':ibdf_combi_pg,'Combi variant-free':ibdf_combi},fdr_levels=[0.01])
-    if 'decoy' in args or 'varpeps' in args:
-        print('FDR re-estimation for variant peptides')
+    plots.plot_scores(ibdf_ontonly_original.dropna(),ibdf_refonly_original.dropna(),ibdf_combi_original.dropna())
+    plots.plot_scores_pg(ibdf_combi_original.dropna(),ibdf_combi_pg_original.dropna())
+    plots.plot_target_decoy(ibdf_combi_original.dropna(),"qc_pearsonr_decoy_varfree.png", plot_title="Search result variant-free")
+    plots.plot_target_decoy(ibdf_combi_pg_original.dropna(),"qc_pearsonr_decoy_varcont.png", plot_title="Search result variant-containing")
+    plots.plot_qvalues_comparison({'ONT only':ibdf_ontonly_original,'Ref only':ibdf_refonly_original,'Combi variant-containing':ibdf_combi_pg_original,'Combi variant-free':ibdf_combi_original},fdr_levels=[0.01])
 
 
     #filter badly scoring hits
-    ibdf_ontonly = file_import.chunk_preprocessing(ibdf_ontonly)
-    ibdf_refonly = file_import.chunk_preprocessing(ibdf_refonly)
-    ibdf_combi = file_import.chunk_preprocessing(ibdf_combi)
-    ibdf_combi_pg= file_import.chunk_preprocessing(ibdf_combi_pg)
+    ibdf_ontonly = file_import.chunk_preprocessing(ibdf_ontonly_original)
+    ibdf_refonly = file_import.chunk_preprocessing(ibdf_refonly_original)
+    ibdf_combi = file_import.chunk_preprocessing(ibdf_combi_original)
+    ibdf_combi_pg= file_import.chunk_preprocessing(ibdf_combi_pg_original)
 
     ##########
     #idea: move data into SQL database and move the quality control plots above to another script to cut down on processing time, since the quality control steps are the only ones that need all, unfiltered data
@@ -57,10 +56,14 @@ def main(args):
     theoretical_saav= calculations.theoretical_saav_counts(mut_cpdt,mut_cpdt_counterparts)
     mut_observed_openmut,mutprotset,chromdist_openmut,stranddist_openmut=main_functions.combidict_analysis(ibdf_combi,chromdict,stranddict,cpdt_pep,full_seqs,theoretical_saav,mut_pep_probs,mut_cpdt,mut_cpdt_counterparts,True)
     mut_observed_classic,chromdist_classic,stranddist_classic=main_functions.combidict_analysis(ibdf_combi_pg,chromdict,stranddict,cpdt_pep,full_seqs,theoretical_saav,mut_pep_probs,mut_cpdt,mut_cpdt_counterparts,False)
-    main_functions.discrepancy_check(mut_observed_classic,mut_observed_openmut, ibdf_combi, ibdf_combi_pg)
+    #here do a re-calculation of FDR for variant peps (look into what functions from combidict_analysis need to be moved to after this recalculation)
+    print("recalculating FDR for variant peptides...")
+    var_vf,var_vc=fdr_reestimation.main(ibdf_combi_original.dropna(),ibdf_combi_pg_original.dropna(),mut_observed_openmut,mut_observed_classic,args["decoy"])
+    print("making final plots...")
+    main_functions.discrepancy_check(var_vc,var_vf, ibdf_combi, ibdf_combi_pg)
     plots.plot_chromosomal_dist(chromdist_classic,chromdist_openmut)
     plots.plot_strand_dist(stranddist_classic,stranddist_openmut)
-    plots.plot_final_venns(mut_observed_classic,mut_observed_openmut,mut_cpdt,mutprotset)
+    plots.plot_final_venns(var_vc,var_vf,mut_cpdt,mutprotset)
     return("Finished")
     
 parser = argparse.ArgumentParser(description='Ionbot output analysis')
@@ -73,8 +76,8 @@ parser.add_argument('--cpdtctp', help='CPDT file of non-mutated counterparts of 
 parser.add_argument('--cpdtvf', help='CPDT file of entire combi variant-free', required=True)
 parser.add_argument('--bed', help='Bed file ONT isoforms', required=True)
 parser.add_argument('--gff', help='Gff3 file GENCODE isoforms', required=True)
-parser.add_argument('--decoy', help='Decoy peptide candidates for FDR re-estimation for variant-containing search',required=False)
-parser.add_argument('--varpeps' help='Scan IDs of variant peptides that were identified as "true" variant peptides')
+parser.add_argument('--decoy', help='Decoy peptide candidates for FDR re-estimation for variant-containing search',required=True)
+# parser.add_argument('--varpeps' help='Scan IDs of variant peptides that were identified as "true" variant peptides')
 args = vars(parser.parse_args()) 
 main(args)
     

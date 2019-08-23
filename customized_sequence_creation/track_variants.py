99f232c0af9c8c811ae688bed91fd935f641a74c
RalfG <rsalz@berkeley.edu>
2019-08-23 16:35:39 +0200

    added some functions

--- customized_sequence_creation/track_variants.py
+++ customized_sequence_creation/track_variants.py
@@ -13,9 +13,7 @@ will return the SNP information (first file) from those variants that were found
 3. get sequence
 '''
 
-def read_filter_cds(cdsfile,observed):
-    prots_unfiltered=observed["proteins"].unique()
-    prots=gather_prots(prots_unfiltered)
+def read_filter_cds(cdsfile,prots):
     cds=pd.DataFrame(columns=["protein","cds"])
     with open(cdsfile) as handle:
         for line in handle:
@@ -30,6 +28,7 @@ def read_filter_cds(cdsfile,observed):
                 if add:
                     cds=cds.append({"protein":header,"cds":seq})
     return(cds)
+
 
 
 def map_seq(reportdf):
@@ -47,9 +46,23 @@ def gather_prots(list_prots):
             out.add(p)
     return out
 
+def protein_filter(prots):
+    o=set()
+    for p in prots:
+        o.add(get_id(p))
+    return(o)
+
+def get_id(idstring):
+    i=idstring
+    if '|m.' in i:
+        i=i.split('|m.')[0]
+    elif 'ENSP' in i:
+        i=i.split('|')[1]
+    return(i)
+
 def main():
     parser = argparse.ArgumentParser(description='track variants')
-    parser.add_argument('--exonvar', help='', required=True)
+    parser.add_argument('--var', help='', required=True)
     parser.add_argument('--cdsont', help='', required=True)
     parser.add_argument('--cdsref', help='', required=True)
     parser.add_argument('--observed', help='', required=True)
@@ -57,8 +70,13 @@ def main():
 
     #read in the observed
     obs=pd.read_csv(args.observed,sep='\t') #proteins \t peptide
-    obs=obs.str.split(',',expand=True).stack()
-    cds_ont=read_filter_cds(args.cdsont,obs) #protein \t cds
-    cds_ref=read_filter_cds(args.cdsref,obs)
-    #need protein to exon mapping- with ref there is 1 line per exon mapping. with ont there is bed12
-    report=read_csv #chr:pos-pos \t chromosomal variant position \t reference allele \t position on the exon sequence \t exon sequence
+    # obs=obs.str.split(',',expand=True).stack()
+    prots_unfiltered=obs["proteins"].unique()
+    prots=gather_prots(prots_unfiltered)
+    cds_ont=read_filter_cds(args.cdsont,prots) #protein \t cds
+    cds_ref=read_filter_cds(args.cdsref,prots)
+    report=pd.read_csv(args.var,sep='\t',columns=["transcript","local_pos","chrom_pos"]) #transcript \t var positions on transcript \t chr|chrom_positions
+    transcripts=protein_filter(prots)
+    report=report.loc[report["transcript"].isin(transcripts)]
+    
+    
